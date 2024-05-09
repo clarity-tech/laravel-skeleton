5240889d8405a28ff960d70e6deb76e38854740b
Manash Sonowal <manash149@gmail.com>
2024-05-09 19:41:31 +0530

    chore: Update AdminPanelProvider.php to use tenant features for by using jetstream teams
    to put all the filament resources under admin namespace
    
    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,9 +2,16 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Common\Pages\ApiTokens;
+use App\Filament\Common\Pages\CreateTeam;
+use App\Filament\Common\Pages\EditProfile;
+use App\Filament\Common\Pages\EditTeam;
+use App\Models\Team;
+use Filament\Facades\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -16,29 +23,48 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Jetstream\Features;
 
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
-        return $panel
+        $panel
             ->default()
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
+            ->passwordReset()
+            ->emailVerification()
+            // ->renderHook('panels::head.start', fn (): string => Vite::useHotFile('admin.hot')
+            //     ->useBuildDirectory('admin')
+            //     ->withEntryPoints(['resources/css/admin.js'])->toHtml()
+            // )
+            ->viteTheme('resources/css/admin.css', 'admin')
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Gray,
             ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('Profile')
+                    ->icon('heroicon-o-user-circle')
+                    ->url(fn () => $this->shouldRegisterMenuItem()
+                        ? url(EditProfile::getUrl())
+                        : url($panel->getPath())),
+            ])
+            ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
+            ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
             ->pages([
                 Pages\Dashboard::class,
+                EditProfile::class,
+                ApiTokens::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -54,5 +80,39 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ]);
+
+        if (Features::hasApiFeatures()) {
+            $panel->userMenuItems([
+                MenuItem::make()
+                    ->label('API Tokens')
+                    ->icon('heroicon-o-key')
+                    ->url(fn () => $this->shouldRegisterMenuItem()
+                        ? url(ApiTokens::getUrl())
+                        : url($panel->getPath())),
+            ]);
+        }
+
+        if (Features::hasTeamFeatures()) {
+
+            $panel
+                ->tenant(Team::class)
+                ->tenantRegistration(CreateTeam::class)
+                ->tenantProfile(EditTeam::class)
+                ->userMenuItems([
+                    MenuItem::make()
+                        ->label('Team Settings')
+                        ->icon('heroicon-o-cog-6-tooth')
+                        ->url(fn () => $this->shouldRegisterMenuItem()
+                            ? url(EditTeam::getUrl())
+                            : url($panel->getPath())),
+                ]);
+        }
+
+        return $panel;
+    }
+
+    public function shouldRegisterMenuItem(): bool
+    {
+        return auth()->user()?->hasVerifiedEmail() && Filament::hasTenancy() && Filament::getTenant();
     }
 }
