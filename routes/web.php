3bd2051174076da5fe912e7a48b19e903d7143ab
Manash Sonowal <manash149@gmail.com>
2024-05-09 17:10:34 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
+    // test ccc
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
